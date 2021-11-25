f8a2e61f9818f64c355bb8f8d537d0df90240d26
joakim.kartveit <joakim.kartveit@edigard.no>
2021-11-25 12:51:23 +0100

    use nav logo

--- src/App.js
+++ src/App.js
@@ -1,18 +1,15 @@
 import React, { Component } from 'react';
 import Countdown from './Countdown.js';
-import logo from './logo.svg';
+import navLogo from './Sort.svg';
 
 class App extends Component {
   render() {
     return (
         <div className="App">
           <div className="App-header">
-            <img src={logo} className="App-logo" alt="logo" />
-            <h2>React Countdown</h2>
-
+            <img src={navLogo} className="Nav-logo" alt="nav logo" />
           </div>
-
-          <h3 className="title">Starting to work at NAV IT (1. februar 2022):</h3>
+          <h3 className="title">Starting to work at NAV IT (1. februar 2022) in</h3>
           <Countdown date={`2022-01-31T00:00:00`} />
         </div>
     );
