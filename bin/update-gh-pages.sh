792452ce3a906ac73493386fa6dd0a8d11d89312
Joakim Taule Kartveit <joakim.kartveit@edigard.no>
2021-11-28 12:32:19 +0100

    Added in username and email

--- bin/update-gh-pages.sh
+++ bin/update-gh-pages.sh
@@ -4,5 +4,5 @@ set -eu
 
 cd "$GITHUB_WORKSPACE"
 
-git config user.name "$GITHUB_ACTOR"
-git config user.email "${GITHUB_ACTOR}@bots.github.com"
+git config user.name "MikAoJk"
+git config user.email "joakimkartveit@gmail.com"
