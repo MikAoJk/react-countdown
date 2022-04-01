e56ca7d0f68cae730b2d614967255df5def5fb39
Joakim Taule Kartveit <joakimkartveit@gmail.com>
2022-04-01 13:30:32 +0200

    Skreiv om til å bli skikkelig typescript

--- src/Countdown.tsx
+++ src/Countdown.tsx
@@ -1,117 +1,112 @@
-import React, {Component} from 'react';
+import React, {useEffect, useState} from 'react';
 
-class Countdown extends Component {
-    static propTypes: { date: string };
-    private interval: NodeJS.Timer | undefined;
+const calculateCountdown = (secondsLeft: number | null) => {
+    if (!secondsLeft) return null
 
-    constructor(props: string) {
-        super(props);
+    const diff = (secondsLeft - new Date().getTime()) / 1000;
 
-        this.state = {
-            days: 0,
-            hours: 0,
-            min: 0,
-            sec: 0,
-        }
+    if (diff <= 0) return null;
+
+    return diff;
+}
+
+interface CalculatedTimeLeft {
+    sec: number;
+    hours: number;
+    min: number;
+    days: number;
+    years: number
+}
+
+const timeLeft = (seconds: number) => {
+    const timeLeft: CalculatedTimeLeft = {
+        years: 0,
+        days: 0,
+        hours: 0,
+        min: 0,
+        sec: 0
+    };
+
+    if (seconds >= (365.25 * 86400)) {
+        timeLeft.years = Math.floor(seconds / (365.25 * 86400));
+        seconds -= timeLeft.years * 365.25 * 86400;
     }
-
-    componentDidMount() {
-        this.interval = setInterval(() => {
-            let date: boolean | { sec: number; hours: number; min: number; days: number; years: number };
-            // TODO remove @ts-ignore
-            // @ts-ignore
-            date = this.calculateCountdown(this.props.date);
-            date ? this.setState(date) : this.stop();
-        }, 1000);
+    if (seconds >= 86400) { // 24 * 60 * 60
+        timeLeft.days = Math.floor(seconds / 86400);
+        seconds -= timeLeft.days * 86400;
     }
-
-    componentWillUnmount() {
-        this.stop();
+    if (seconds >= 3600) { // 60 * 60
+        timeLeft.hours = Math.floor(seconds / 3600);
+        seconds -= timeLeft.hours * 3600;
     }
-
-    calculateCountdown(endDate: Date) {
-        let diff = (Date.parse(new Date(endDate).toString()) - Date.parse(new Date().toString())) / 1000;
-
-        if (diff <= 0) return false;
-
-        const timeLeft = {
-            years: 0,
-            days: 0,
-            hours: 0,
-            min: 0,
-            sec: 0
-        };
-
-        if (diff >= (365.25 * 86400)) {
-            timeLeft.years = Math.floor(diff / (365.25 * 86400));
-            diff -= timeLeft.years * 365.25 * 86400;
-        }
-        if (diff >= 86400) { // 24 * 60 * 60
-            timeLeft.days = Math.floor(diff / 86400);
-            diff -= timeLeft.days * 86400;
-        }
-        if (diff >= 3600) { // 60 * 60
-            timeLeft.hours = Math.floor(diff / 3600);
-            diff -= timeLeft.hours * 3600;
-        }
-        if (diff >= 60) {
-            timeLeft.min = Math.floor(diff / 60);
-            diff -= timeLeft.min * 60;
-        }
-        timeLeft.sec = diff;
-
-        return timeLeft;
+    if (seconds >= 60) {
+        timeLeft.min = Math.floor(seconds / 60);
+        seconds -= timeLeft.min * 60;
     }
+    timeLeft.sec = seconds;
 
-    stop() {
-        clearInterval(this.interval as NodeJS.Timeout);
-    }
+    return timeLeft;
+}
 
-    addLeadingZeros(value: string | any[]) {
-        value = String(value);
-        while (value.length < 2) {
-            value = '0' + value;
+
+interface CountdownProps {
+    date: string;
+}
+
+const addLeadingZeros = (value: number) => {
+    const stringValue = String(value);
+    return stringValue.length < 2 ? '0' + stringValue : stringValue;
+}
+
+export const Countdown = ({date}: CountdownProps) => {
+    const parsedDate = Date.parse(date);
+
+    const [secondsLeft, setSecondsLeft] = useState(parsedDate)
+
+    useEffect(() => {
+            const interval = setInterval(() => {
+                const tempCountdownDate: number | null = calculateCountdown(secondsLeft);
+                tempCountdownDate && setSecondsLeft(tempCountdownDate);
+            }, 1000);
+            return () => clearInterval(interval)
         }
-        return value;
-    }
+        , []
+    )
 
-    render() {
-        // TODO remove @ts-ignore
-        // @ts-ignore
-        const {days, hours, min, sec} = this.state;
+    const {days, hours, min, sec} = timeLeft(secondsLeft);
 
-        return (
-            <div className="Countdown">
+    return (
+        <div className="Countdown">
         <span className="Countdown-col">
           <span className="Countdown-col-element">
-              <strong>{this.addLeadingZeros(days)}</strong>
+              <strong>{addLeadingZeros(days)}</strong>
               <span>{days === 1 ? 'Day' : 'Days'}</span>
           </span>
         </span>
 
-                <span className="Countdown-col">
+            <span className="Countdown-col">
           <span className="Countdown-col-element">
-            <strong>{this.addLeadingZeros(hours)}</strong>
+            <strong>{addLeadingZeros(hours)}</strong>
             <span>Hours</span>
           </span>
         </span>
 
-                <span className="Countdown-col">
+            <span className="Countdown-col">
           <span className="Countdown-col-element">
-            <strong>{this.addLeadingZeros(min)}</strong>
+            <strong>{addLeadingZeros(min)}</strong>
             <span>Min</span>
           </span>
         </span>
 
-                <span className="Countdown-col">
+            <span className="Countdown-col">
           <span className="Countdown-col-element">
-            <strong>{this.addLeadingZeros(sec)}</strong>
+            <strong>{addLeadingZeros(Math.floor(sec))}</strong>
             <span>Sec</span>
           </span>
         </span>
-            </div>
-        );
-    }
+        </div>
+    );
+
 }
 
 export default Countdown;
